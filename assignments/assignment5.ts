4b071b8bd979421b629f67a035e354a9e92c7a61
sivakumar s <ssivak.tech@gmail.com>
2026-02-17 19:08:40 +0530

    assignment5updated

--- assignments/assignment5.ts
+++ assignments/assignment5.ts
@@ -1,21 +1,9 @@
-/*Map ==> Map represents a collection of key-value pairs. 
-let empInfoMap:Map<number,string> = new Map();
-empInfoMap.set(123,"Bharath");
-empInfoMap.set(124,"Sarath");
-//Set ==> Set represents list of unique values. 
-let empIds:Set<number> = new Set();
-empIds.add(123);
-empIds.add(124)
 
- empName:'Alice Johnson',baseSalary:number,exp:number,rating:number
-Alice
-Johnson 75000.0 5.1 4.2
-2 Bob Smith 68000.0 3.2 3.8
-3 Carol Davis 82000.0 7.1 4.5
-4 David Brown 90000.0 10.2 2.5
-5 Eva Green 60000.0 2.4 3.5
-*/
+/*Now Calculate the Hike percentage of each Employee and store the
+Values in Map with EmployeeName and HikePercentagevalue and Print them.*/
+
 let empHikePercent:Map<string,number> = new Map();
+
 interface EmployeeTable{
     empName:string,
     empBaseSalary:number,
@@ -31,39 +19,47 @@ let employeeTable:EmployeeTable[]=[
     {empName:'Eva Green',empBaseSalary:60000,empExp:2.4,empRating:3.5,hikePercent:0},
 ]
 
-
-//console.log(75000+((75000*15)/100));
-function getEmpHikePercent(empData:EmployeeTable[]):EmployeeTable[]{
+function getEmpHikePercent(empData:EmployeeTable[]):Map<string,number>{
    
     let hike:number=0;
 
     for(let val of empData){
         if (val.empRating>=4)
             {
-                hike=val.empBaseSalary+((15*val.empBaseSalary)/100)+1500;
+                hike=val.empBaseSalary*(15/100)+1500;
                 if(val.empExp>=5){
                      hike+=5000;
                 }
-               val.hikePercent=hike/val.empBaseSalary;
-                
+               val.hikePercent=(hike/val.empBaseSalary)*100;  
+               empHikePercent.set(val.empName,val.hikePercent);
             }
             else if(val.empRating>=3)
                 {
-                    hike=(val.empBaseSalary+((10*val.empBaseSalary)/100)+1200);
+                    hike=val.empBaseSalary*(10/100)+1200;
                       if(val.empExp>=5){
                         hike+=5000;
                     }
-                     val.hikePercent=hike/val.empBaseSalary;
+                     val.hikePercent=(hike/val.empBaseSalary)*100;
+                     empHikePercent.set(val.empName,val.hikePercent);
                  }
                  else{
-                    hike=val.empBaseSalary+((3*val.empBaseSalary)/100)+300;
+                    hike=val.empBaseSalary*(3/100)+300;
                      if(val.empExp>=5){
                         hike+=5000;
                     }
-                    val.hikePercent=hike/val.empBaseSalary; 
+                    val.hikePercent=(hike/val.empBaseSalary)*100;
+                    empHikePercent.set(val.empName,val.hikePercent); 
                  }
                 }
-                return empData;
+                return empHikePercent;
             }
-
 console.log(getEmpHikePercent(employeeTable));
+/**************************************
+Output:
+Map(5) {
+  'Alice Johnson' => 23.666666666666668,
+  'Bob Smith' => 11.76470588235294,
+  'Carol Davis' => 22.926829268292686,
+  'David Brown' => 8.88888888888889,
+  'Eva Green' => 12
+}***************************************/
