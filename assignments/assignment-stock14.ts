228b185ef021f86fd65abdff19f8a01de1e974d4
sivakumar s <ssivak.tech@gmail.com>
2026-03-15 12:39:44 +0530

    modified

--- assignments/assignment-stock14.ts
+++ assignments/assignment-stock14.ts
@@ -20,29 +20,43 @@ Explanation: In this case, no transactions are done and the max profit = 0.*/
 //find the minprice of all day
 //find max price of all day after minprice day,
 //if no max price after minprice day - profit is zero
-//if max price day after min price day, maxprice-min price =profit
+//if max price day after min price da4y, maxprice-min price =profit
 //buy date= min price date, sell date=max price date.
 
-const stockPrice: number[] = [123, 1, 11, 12, 4, 99];
+//const stockPrice: number[] = [123, 1, 11, 12, 4, 99];
+//const stockPrice: number[] = [7,6,4,3,1];
+//const stockPrice: number[] =  [7,6,1,1,5];
+const stockPrice: number[] =[7,1,5,3,6,4] 
+//const stockPrice: number[] =[7,6,4,3,1] 
+//const stockPrice: number[] =[1,2];
+//const stockPrice: number[] =[2,1] 
+//const stockPrice: number[] =[1,1,1]
 
-let buyday:number=0;
-let sellday:number=1;
-let profit:number=0;
-let tempProfit:number=0;
-let buyPrice:number=0;
-let sellPrice:number=0;
 
-for(let i:number=1;i<stockPrice.length;i++){
-    if(stockPrice[sellday]>stockPrice[buyday]){
-      tempProfit=stockPrice[sellday]-stockPrice[buyday];
-      buyPrice=stockPrice[buyday];
-      sellPrice=stockPrice[sellday]
-       if(profit<tempProfit){profit=tempProfit}
+let currentDay: number = 0;
+let nextDay: number = 1;
+let profit: number = 0;
+let tempProfit: number = 0;
+let buyPrice: number = 0;
+let sellPrice: number = 0;
+//let buyDay:number=0;
+//let sellDay:number=0;
+
+for (let i: number = 0; i < stockPrice.length-1; i++) {
+    if (stockPrice[nextDay] > stockPrice[currentDay]) {
+        tempProfit = stockPrice[nextDay] - stockPrice[currentDay];
+        if (profit < tempProfit) {
+            profit = tempProfit;
+            buyPrice = stockPrice[currentDay];
+            sellPrice = stockPrice[nextDay];
+            //buyDay=currentDay;
+            //sellDay=nextDay;
+        }
     }
-    else{buyday=sellday;}
-    sellday++;
+    else { currentDay = nextDay; }
+    nextDay++;
 }
-console.log('BuyPrice:',buyPrice+" SellPrice:",sellPrice+" Profit:",profit);
+console.log('BuyPrice:', buyPrice + " SellPrice:", sellPrice + " Profit:", profit);// + " Buydate:", buyDay + " Selldate:", sellDay);
 
 
 
